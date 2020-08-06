d62600e521ebc2fe1ac835f838e995558ad8b5f7
Robert Björklund <deliro257@gmail.com>
2020-08-06 08:29:43 +0200

    Story #12345 some comment

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
-import { Observable } from "rxjs";
-var observable = Observable.create((observer: any) => {
-  observer.next("Hello World!");
-  observer.next("Hello Again!");
-  observer.complete();
-  observer.next("Bye");
-});
-observable.subscribe(
-  (x: any) => logItem(x),
-  (error: any) => logItem("Error: " + error),
-  () => logItem("Completed")
-);
-function logItem(val: any) {
-  var node = document.createElement("li");
-  var textnode = document.createTextNode(val);
-  node.appendChild(textnode);
-  document.getElementById("list").appendChild(node);
-}
+import { Observable, fromEvent } from "rxjs";
+
+const carSelector = <HTMLSelectElement>document.getElementById("cars");
+
+var select$ = fromEvent(carSelector, "change");
+
+select$.subscribe((x) => console.log(carSelector.value));
